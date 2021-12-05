1ed4601fd81a199b4daa688b6fb734c096533c18
Ricky Bustillos <henrique.ssbl@hotmail.com>
2021-12-04 21:56:40 -0300

    dev(card): base card & assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import 'vanilla-tilt'; 
+declare var VanillaTilt: any; 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ss-landing-page';
+export class AppComponent implements OnInit {
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    VanillaTilt.init(document.querySelectorAll(".tilt"));
+  }
+
+  joinServer() {
+    this.router.navigateByUrl('https://discord.gg/J3tvqde');
+  }
 }
